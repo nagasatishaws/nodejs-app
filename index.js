c14e67f3449a8a54223815049d920137da59dbfb
mrvitej786 <mrvitej.aws@gmail.com>
2024-07-17 22:09:30 +0530

    5

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('hi this is devops-sessions by nagasatish and my student has an mani he has an devops ')
+  response.send('hi this is devops-sessions by nagasatish and my student has an mani he has an devops ?')
 }) 
 
 app.listen(app.get('port'), function() {
